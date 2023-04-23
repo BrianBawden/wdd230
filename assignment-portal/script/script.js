d4d35e26e5f4d18f0009bce384d37d659c6d571f
Brian Bawden <brianbawden001@gmail.com>
2023-04-23 18:02:55 -0400

    formated index.html, script.js and style.css.

--- assignment-portal/script/script.js
+++ assignment-portal/script/script.js
@@ -2,18 +2,15 @@ document.querySelector("#copyYear").innerHTML = getYear();
 
 document.querySelector("#modDate").innerHTML = getMY();
 
-
-function getYear(){
-
-    return new Date().getFullYear(); 
+function getYear() {
+  return new Date().getFullYear();
 }
 
-function getMY(){
-   
-    const lastModif = new Date(document.lastModified);
-    const mMonth = lastModif.toLocaleString("default", {month: "long"});
-    const mYear = lastModif.getFullYear();
-    const date = `${mMonth} ${mYear}`;
+function getMY() {
+  const lastModif = new Date(document.lastModified);
+  const mMonth = lastModif.toLocaleString("default", { month: "long" });
+  const mYear = lastModif.getFullYear();
+  const date = `${mMonth} ${mYear}`;
 
-    return date;
+  return date;
 }
