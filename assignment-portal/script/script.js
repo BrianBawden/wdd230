f1f37f0536302db1ad8e54a26dd0e98c878d05fb
Brian Bawden <brianbawden001@gmail.com>
2023-04-23 17:44:41 -0400

    add functions to get dates for copyright and modified.

--- assignment-portal/script/script.js
+++ assignment-portal/script/script.js
@@ -1,3 +1,19 @@
-document.querySelector("#copyYear").innerHTML = 2023;
+document.querySelector("#copyYear").innerHTML = getYear();
 
-document.querySelector("#modDate").innerHTML = "April 2023";
+document.querySelector("#modDate").innerHTML = getMY();
+
+
+function getYear(){
+
+    return new Date().getFullYear(); 
+}
+
+function getMY(){
+   
+    let lastModif = new Date(document.lastModified);
+    let mMonth = lastModif.toLocaleString("default", {month: "long"});
+    let mYear = lastModif.getFullYear();
+    let date = mMonth + " " + mYear;
+
+    return date;
+}
