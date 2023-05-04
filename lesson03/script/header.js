de073e29fe27979c55dce252bfec73faa9418717
Brian Bawden <brianbawden001@gmail.com>
2023-05-04 14:16:08 -0400

    changed html header to js header for concistance in webpage.

---
+++ lesson03/script/header.js
@@ -0,0 +1,2 @@
+document.querySelector("header").innerHTML =
+  '<img src="images/logo.png" alt="mountain top logo"> <h1><img > Chamber of Commerce</h1><ul>    <li><a href="index.html">Home</a></li>    <li><a href="target-audience.html">Target Audience</a></li>    <li><a href="design.html">Design</a></li></ul>';
