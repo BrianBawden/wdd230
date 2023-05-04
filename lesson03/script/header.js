9c9573f550d3838dd0a3994d86405ef9b165a7f1
Brian Bawden <brianbawden001@gmail.com>
2023-05-04 14:45:53 -0400

    cleaned up header.js to more readable.

--- lesson03/script/header.js
+++ lesson03/script/header.js
@@ -1,2 +1,10 @@
 document.querySelector("header").innerHTML =
-  '<img src="images/logo.png" alt="mountain top logo"> <h1><img > Chamber of Commerce</h1><ul>    <li><a href="index.html">Home</a></li>    <li><a href="target-audience.html">Target Audience</a></li>    <li><a href="design.html">Design</a></li></ul>';
+  `
+  <img src="images/logo.png" alt="mountain top logo">
+  <h1><img > Chamber of Commerce</h1>
+  <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="target-audience.html">Target Audience</a></li>
+      <li><a href="design.html">Design</a></li>
+  </ul>
+  `;
