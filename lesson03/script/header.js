364eb75c9e47ed95c5cf914ca3f68cd0dea73667
Brian Bawden <brianbawden001@gmail.com>
2023-05-05 14:43:09 -0400

    change header links from <ul> to <div>/<p>.

--- lesson03/script/header.js
+++ lesson03/script/header.js
@@ -1,11 +1,11 @@
 document.querySelector("header").innerHTML =
   `
    <h1><img src="images/logo.png" alt="mountain top logo"> Chamber of Commerce</h1>
-  <ul>
-      <li><a href="index.html"> Home </a></li>
-      <li><a href="target-audience.html"> Target Audience </a></li>
-      <li><a href="design.html"> Design </a></li>
-  </ul>
+  <div class="links">
+      <p><a href="index.html"> Home </a></p>
+      <p><a href="target-audience.html"> Target Audience </a></p>
+      <p><a href="design.html"> Design </a></p>
+  </div>
   `;
 
 
