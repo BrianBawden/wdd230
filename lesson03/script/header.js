d0dc77eb55ddd93ffb0e0e3671c2dba11796a33d
Brian Bawden <brianbawden001@gmail.com>
2023-05-05 13:55:17 -0400

    change color schema around and change logo.

--- lesson03/script/header.js
+++ lesson03/script/header.js
@@ -1,10 +1,15 @@
 document.querySelector("header").innerHTML =
   `
-  <img src="images/logo.png" alt="mountain top logo">
-  <h1> Chamber of Commerce</h1>
+   <h1><img src="images/logo.png" alt="mountain top logo"> Chamber of Commerce</h1>
   <ul>
-      <li><a href="index.html">Home</a></li>
-      <li><a href="target-audience.html">Target Audience</a></li>
-      <li><a href="design.html">Design</a></li>
+      <li><a href="index.html"> Home </a></li>
+      <li><a href="target-audience.html"> Target Audience </a></li>
+      <li><a href="design.html"> Design </a></li>
   </ul>
   `;
+
+
+  document.querySelector("footer").innerHTML =
+  `
+  
+  `
