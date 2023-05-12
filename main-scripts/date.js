ec24a1fdbfc2dc3808d70fb3c182c4a85c0bfe8c
Brian Bawden <brianbawden001@gmail.com>
2023-05-11 20:21:29 -0400

    init script.js.

--- main-scripts/date.js
+++ main-scripts/date.js
@@ -1,6 +1,14 @@
+const now = new Date();
+
+const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
 document.querySelector("#copyYear").innerHTML = getYear();
 
 document.querySelector("#modDate").innerHTML = getMod();
+
+document.querySelector(".curr-date").innerHTML = fullDate;
 
 function getYear() {
   return new Date().getFullYear();
@@ -10,3 +18,4 @@ function getMod() {
     return new Date(document.lastModified);
 
 }
+
