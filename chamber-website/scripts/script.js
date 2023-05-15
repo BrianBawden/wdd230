6afca57df6db0e6e7fe98ffdebde718b82277d7a
Brian Bawden <brianbawden001@gmail.com>
2023-05-15 18:31:59 -0400

    format chamber-website files.

--- chamber-website/scripts/script.js
+++ chamber-website/scripts/script.js
@@ -1,4 +1,4 @@
-function toggleMenu(){
-    console.log("test")
-    document.querySelector(".nav").classList.toggle("open");
+function toggleMenu() {
+  console.log("test");
+  document.querySelector(".nav").classList.toggle("open");
 }
