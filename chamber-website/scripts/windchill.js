5c122c91f23ebb8351db1a390c350147d4af85d0
Brian Bawden <brianbawden001@gmail.com>
2023-05-23 15:51:01 -0400

    add if statment for windchill.

--- chamber-website/scripts/windchill.js
+++ chamber-website/scripts/windchill.js
@@ -1,10 +1,19 @@
 const tempDoc = document.querySelector("#temp").textContent;
 const windSpeedDoc = document.querySelector("#windSpeed").textContent;
+const chillDoc = document.querySelector("#windChill");
+ 
 
-windChill(tempDoc, windSpeedDoc);
+
+if (tempDoc <= 50 & windSpeedDoc > 3){
+    windChill(tempDoc, windSpeedDoc);
+}
+else{
+    chillDoc.innerHTML = "N/A"
+
+}
 
 function windChill(t, s){
     let windChill = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * Math.pow(t, 0.16));
 
-    document.querySelector("#windChill").innerHTML = windChill.toFixed(0);
+    chillDoc.innerHTML = windChill.toFixed(0);
 }
