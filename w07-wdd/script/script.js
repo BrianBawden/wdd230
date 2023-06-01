b09999019e1b7248e413b707f4a6959b07a0f11c
Brian Bawden <brianbawden001@gmail.com>
2023-06-01 15:04:37 -0400

    change numImg to length of imgsReal.

--- w07-wdd/script/script.js
+++ w07-wdd/script/script.js
@@ -1,4 +1,3 @@
-const numImg = 6;
 const imgs = document.querySelector(".imgs");
 const imgsReal = ["images/img1.jpg", 
                   "images/img2.jpg",
@@ -6,21 +5,22 @@ const imgsReal = ["images/img1.jpg",
                   "images/img4.jpg",
                   "images/img5.jpg",
                   "images/img6.jpg"]
+const numImg = imgsReal.length;
 
-// let imagesToLoad = document.querySelectorAll("img[data-src]");
-// const loadImages = (image) => {
-//     image.setAttribute(src, image.getAttribute("data-src"));
-//     image.onload = () => {
-//         image.removeAttribute("data-src");
-//     };
-// };
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+    image.setAttribute(src, image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
 
 
 for(let i = 0; i < numImg; i++){
     const imgLi = document.createElement("li");
 
-    imgLi.innerHTML = "<img src='" + imgsReal[1] + "' data-src=" + imgsReal[i] + "> ";
-    // imgLi.innerHTML = "<img src='" + imgsReal[1] + "'>";
+    imgLi.innerHTML = "<img src='https://placehold.jp/300x400.png' data-src=" + imgsReal[i] + "> ";
+    "<img src='https://placehold.jp/300x400.png'> "
 
     imgs.appendChild(imgLi);
 }
