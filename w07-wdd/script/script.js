0cd7c55844301754d61fc8f41edc6b9dfd6fe0f9
Brian Bawden <brianbawden001@gmail.com>
2023-06-01 15:14:07 -0400

    working on intersection observer.

--- w07-wdd/script/script.js
+++ w07-wdd/script/script.js
@@ -1,21 +1,21 @@
-const imgs = document.querySelector(".imgs");
+const imgs = document.querySelector(".imgs"); // unordered list
 const imgsReal = ["images/img1.jpg", 
                   "images/img2.jpg",
                   "images/img3.jpg",
                   "images/img4.jpg",
                   "images/img5.jpg",
-                  "images/img6.jpg"]
-const numImg = imgsReal.length;
+                  "images/img6.jpg"] // file names of the images used
+const numImg = imgsReal.length; // length used for the for loop
 
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 const loadImages = (image) => {
-    image.setAttribute(src, image.getAttribute("data-src"));
+    image.setAttribute('src', image.getAttribute("data-src"));
     image.onload = () => {
         image.removeAttribute("data-src");
     };
 };
 
-
+// this for loop adds the li's to ul based on the length of imgsReal.
 for(let i = 0; i < numImg; i++){
     const imgLi = document.createElement("li");
 
@@ -23,6 +23,14 @@ for(let i = 0; i < numImg; i++){
     "<img src='https://placehold.jp/300x400.png'> "
 
     imgs.appendChild(imgLi);
+}; 
+
+if("IntersectionObserver" in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            
+        })
+    })
 }
 
 imagesToLoad.forEach((img) => {
