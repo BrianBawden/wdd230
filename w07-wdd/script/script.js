f04ddb0c28794381d7e65287fd1c3998ff2e2a9c
Brian Bawden <brianbawden001@gmail.com>
2023-05-31 22:12:50 -0400

    Add array of path to images.

--- w07-wdd/script/script.js
+++ w07-wdd/script/script.js
@@ -1,5 +1,6 @@
 const numImg = 6;
 const imgs = document.querySelector(".imgs");
+const imgsReal = ["images/img1.png", "images/img2.png", "images/img3.png", "images/img4.png", "images/img5.png", "images/img6.png"]
 
 
 
