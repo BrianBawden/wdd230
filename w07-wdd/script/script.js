0f34e89ae00b5fcf4d21b1d8453fccd18525baff
Brian Bawden <brianbawden001@gmail.com>
2023-06-01 14:55:04 -0400

    imasReal wrong image type, corrected to .jpg.

--- w07-wdd/script/script.js
+++ w07-wdd/script/script.js
@@ -1,25 +1,27 @@
 const numImg = 6;
 const imgs = document.querySelector(".imgs");
-const imgsReal = ["images/img1.png", 
-                  "images/img2.png",
-                  "images/img3.png",
-                  "images/img4.png",
-                  "images/img5.png",
-                  "images/img6.png"]
+const imgsReal = ["images/img1.jpg", 
+                  "images/img2.jpg",
+                  "images/img3.jpg",
+                  "images/img4.jpg",
+                  "images/img5.jpg",
+                  "images/img6.jpg"]
 
-let imagesToLoad = document.querySelectorAll("img[data-src]");
-const loadImages = (image) => {
-    image.setAttribute(src, image.getAttribute("data-src"));
-    image.onload = () => {
-        image.removeAttribute("data-src");
-    };
-};
+// let imagesToLoad = document.querySelectorAll("img[data-src]");
+// const loadImages = (image) => {
+//     image.setAttribute(src, image.getAttribute("data-src"));
+//     image.onload = () => {
+//         image.removeAttribute("data-src");
+//     };
+// };
 
 
 for(let i = 0; i < numImg; i++){
     const imgLi = document.createElement("li");
 
     imgLi.innerHTML = "<img src='" + imgsReal[1] + "' data-src=" + imgsReal[i] + "> ";
+    // imgLi.innerHTML = "<img src='" + imgsReal[1] + "'>";
+
     imgs.appendChild(imgLi);
 }
 
