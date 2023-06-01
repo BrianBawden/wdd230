a1bfffeff0aa8bc59cce6d5a750542cc902c073c
Brian Bawden <brianbawden001@gmail.com>
2023-06-01 14:29:28 -0400

    working on getting data-src working.

--- w07-wdd/script/script.js
+++ w07-wdd/script/script.js
@@ -1,12 +1,28 @@
 const numImg = 6;
 const imgs = document.querySelector(".imgs");
-const imgsReal = ["images/img1.png", "images/img2.png", "images/img3.png", "images/img4.png", "images/img5.png", "images/img6.png"]
+const imgsReal = ["images/img1.png", 
+                  "images/img2.png",
+                  "images/img3.png",
+                  "images/img4.png",
+                  "images/img5.png",
+                  "images/img6.png"]
 
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+    image.setAttribute(src, image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
 
 
 for(let i = 0; i < numImg; i++){
     const imgLi = document.createElement("li");
 
-    imgLi.innerHTML = "<img src='https://placehold.jp/300x400.png'> ";
+    imgLi.innerHTML = "<img src=" + imgsReal[1] + " data-src=" + imgsReal[i] + "> ";
     imgs.appendChild(imgLi);
 }
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
