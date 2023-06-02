8b18b60e61ac576720d9b29d0e1fc1102a442a7a
Brian Bawden <brianbawden001@gmail.com>
2023-06-02 09:36:23 -0400

    add loading lazy to imgs.

--- w07-wdd/script/script.js
+++ w07-wdd/script/script.js
@@ -7,7 +7,22 @@ const imgsReal = ["images/img1.jpg",
                   "images/img6.jpg"] // file names of the images used
 const numImg = imgsReal.length; // length used for the for loop
 
+// this for loop adds the li's to ul based on the length of imgsReal.
+for(let i = 0; i < numImg; i++){
+    const imgLi = document.createElement("li");
+
+    imgLi.innerHTML = "<img src='https://placehold.jp/300x400.png' data-src=" + imgsReal[i] + " loading='lazy'> ";
+
+    imgs.appendChild(imgLi);
+}; 
+
 let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute("data-src"));
     image.onload = () => {
@@ -15,24 +30,16 @@ const loadImages = (image) => {
     };
 };
 
-// this for loop adds the li's to ul based on the length of imgsReal.
-for(let i = 0; i < numImg; i++){
-    const imgLi = document.createElement("li");
-
-    imgLi.innerHTML = "<img src='https://placehold.jp/300x400.png' data-src=" + imgsReal[i] + "> ";
-    "<img src='https://placehold.jp/300x400.png'> "
-
-    imgs.appendChild(imgLi);
-}; 
-
-if("IntersectionObserver" in window) {
-    const imgObserver = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            
-        })
-    })
-}
 
 imagesToLoad.forEach((img) => {
     loadImages(img);
 });
+
+if("IntersectionObserver" in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            loadImages(item);
+
+        })
+    })
+}
