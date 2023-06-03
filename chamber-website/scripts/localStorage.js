b5cdc79141162c9b14540be5ec86f31a943e44b1
Brian Bawden <brianbawden001@gmail.com>
2023-06-03 13:48:02 -0400

    init localStorage.js

---
+++ chamber-website/scripts/localStorage.js
@@ -0,0 +1,15 @@
+let getNow = new Date();
+const millPerDay = 1000 * 60 * 60 * 24;
+
+
+localStorage.setItem("visitDate", getNow.getTime());
+
+const lastVisit = localStorage.getItem("visitDate");
+
+const newDate = getNow.getTime();
+const oldDate = lastVisit;
+const millDiff = newDate - oldDate;
+
+const numDays = millDiff / millPerDay;
+
+
