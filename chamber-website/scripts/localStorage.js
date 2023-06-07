d37fe47bb31a1e34955c64587ce390d51a159b9d
Brian Bawden <brianbawden001@gmail.com>
2023-06-07 12:03:55 -0400

    fix logic for localStorage.js.

--- chamber-website/scripts/localStorage.js
+++ chamber-website/scripts/localStorage.js
@@ -1,15 +1,21 @@
+// Three days ago: 1685892771985
+
+//******************* */
 let getNow = new Date();
+let lastVisit = localStorage.getItem("visitDate");
+// let oldDate;
 const millPerDay = 1000 * 60 * 60 * 24;
-
-
-localStorage.setItem("visitDate", getNow.getTime());
-
-const lastVisit = localStorage.getItem("visitDate");
+if(lastVisit !== null){
+    lastVisit = localStorage.getItem("visitDate");
+}
+if (lastVisit === null){
+    lastVisit = getNow;
+}
 
 const newDate = getNow.getTime();
-const oldDate = lastVisit;
-const millDiff = newDate - oldDate;
+const millDiff = newDate - lastVisit;
 
 const numDays = millDiff / millPerDay;
 
-document.querySelector("#day").textContent = numDays;
+document.querySelector("#day").textContent = numDays.toFixed(0);
+lastVisit = localStorage.setItem("visitDate", getNow.getTime());
