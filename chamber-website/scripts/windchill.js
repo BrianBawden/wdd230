258c309ecfee4b1cb95f42b07888277bc6f88207
Brian Bawden <brianbawden001@gmail.com>
2023-06-23 09:46:36 -0400

    access weather api and show current temp, icon, and description.

--- chamber-website/scripts/windchill.js
+++ chamber-website/scripts/windchill.js
@@ -1,6 +1,12 @@
-const tempDoc = document.querySelector("#temp").textContent;
-const windSpeedDoc = document.querySelector("#windSpeed").textContent;
+const tempDoc = document.querySelector("#temp");
+const windSpeedDoc = document.querySelector("#windSpeed");
 const chillDoc = document.querySelector("#windChill");
+const wCondition = document.querySelector("#wCondition");
+const wIcon = document.querySelector("#wIcon");
+
+// url for openweather api
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=40.2970&lon=-111.6968&units=imperial&appid=b288360203608f2b768739c3ac102db2";
 
 if ((tempDoc <= 50) & (windSpeedDoc > 3)) {
   windChill(tempDoc, windSpeedDoc);
@@ -14,3 +20,34 @@ function windChill(t, s) {
 
   chillDoc.innerHTML = windChill.toFixed(0);
 }
+
+
+// ********** API code for weather. **************
+
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(wData) {
+  const iconsrc = `https://openweathermap.org/img/w/${wData.weather[0].icon}.png`;
+  const windSpeed = wData.wind.speed;
+  tempDoc.innerHTML = wData.main.temp.toFixed(0);
+  wCondition.innerHTML = wData.weather[0].description;
+  wIcon.setAttribute("src", iconsrc);
+
+  console.log(windSpeed);
+}
+
+apiFetch();
