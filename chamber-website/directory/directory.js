077cf583ee377f0179b28d9a546d4b85b124cce4
Brian Bawden <brianbawden001@gmail.com>
2023-06-29 12:15:51 -0400

    arrayGold holds three objects from fetBiz.

--- chamber-website/directory/directory.js
+++ chamber-website/directory/directory.js
@@ -3,19 +3,27 @@ const biz = 'directory/data.json';
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.querySelector(".cards");
+const arrayGold = [];
+let dirLength;
 let bizData;
+
+// ************* get json **********************
 async function getBiz(){
     const response = await fetch(biz);
     const data = await response.json();
-    displayBiz(data.bizz)
+    displayBiz(data.bizz);
+    const hiLi = highlightBiz(data.bizz);
+    console.log(arrayGold);
 }
 
 
-
+// ************* create and set cards ****************
 const displayBiz = (businesses) => {
     const cards = document.querySelector("div.cards");
     // for each biz create their content.
     businesses.forEach((business) => {
+    //  console.log(business.memberLvl);
+
         let card = document.createElement("section");
         let icon = document.createElement('img');
         let bizName = document.createElement('h3');
@@ -40,6 +48,8 @@ const displayBiz = (businesses) => {
     });
 }
 
+
+// **************** buttons to switch from grid to list view ****************
 gridbutton.addEventListener("click", () => {
 	display.classList.remove("list");
 });
@@ -50,4 +60,32 @@ function showList() {
 	display.classList.add("list");
 }
 
+// ***************** pick random gold/silver card ***************
+
+function goldRandom(data) {
+    dirLength = data.length;
+    let dirRandom = Math.floor(Math.random() * (dirLength));
+    let member = data[dirRandom].memberLvl;
+
+
+    if ((member === "gold" || member === "silver") && !arrayGold.includes(data[dirRandom])) {
+        let hiObj = data[dirRandom];
+        return hiObj;
+       }
+    else{
+        return goldRandom(data);
+    };
+}
+
+function highlightBiz(data) {
+    if (arrayGold.length != 3) {
+        let hiObj = goldRandom(data);
+        arrayGold.push(hiObj);
+        highlightBiz(data);
+    } else {
+        return arrayGold;
+    }
+}
+
+// *************** call function to start program *******************
 getBiz();
