157592d432a1ef44c983b7c789e9cfffa2a3aebe
Brian Bawden <brianbawden001@gmail.com>
2023-07-07 23:00:49 -0400

    set up header in js file and add img for index.html.

---
+++ final/scripts/header.js
@@ -0,0 +1,26 @@
+
+document.querySelector("header").innerHTML = `
+
+<div class="logo">
+  <picture
+    ><img
+      class="icon"
+      src="images/socialMedia/logo.png"
+      alt="Carlsbad Fruit Stand logo"
+    />
+  </picture>
+</div>
+<ul class="nav response">
+  <li><a href="#" onclick="toggleMenu()">&#9776;</a></li>
+  <li><a href="index.html">Home</a></li>
+  <li><a href="pages/fresh.html">Fresh</a></li>
+  <li><a href="pages/about.html">About Us</a></li>
+  <li><a href="pages/attribute.html">Attribute</a></li>
+  
+</ul>
+
+  `;
+
+  document.querySelector("footer").innerHTML = `
+  <h1>Test</h1>
+  `;
