be269402b4c6d0e2cf21eb9962f7f757f93581ca
Brian Bawden <brianbawden001@gmail.com>
2023-07-16 18:02:16 -0400

    finish select three fruits from json.

--- final/scripts/drinks.js
+++ final/scripts/drinks.js
@@ -1,21 +1,43 @@
 // ********* get fruit.json from stuff **********
 const fruitJson = '../stuff/fruit.json';
+const selectFruit1 = document.querySelector("#fruit1");
+const selectFruit2 = document.querySelector("#fruit2");
+const selectFruit3 = document.querySelector("#fruit3");
+let ArrayFruit = [];
+
 
 async function getFruit(){
     const response = await fetch(fruitJson);
     const fruitData = await response.json();
-    console.log(fruitData);
+
+    fruitData.forEach(fruit => {
+        // Create a new option element for each select element
+        let option1 = document.createElement('option');
+        let option2 = document.createElement('option');
+        let option3 = document.createElement('option');
+      
+        // Set the value and text of the option element
+        option1.value = fruit.name;
+        option1.text = fruit.name;
+        option2.value = fruit.name;
+        option2.text = fruit.name;
+        option3.value = fruit.name;
+        option3.text = fruit.name;
+      
+        // Append the option elements to the respective select elements
+        selectFruit1.appendChild(option1);
+        selectFruit2.appendChild(option2);
+        selectFruit3.appendChild(option3);
+      });
+      
 }
-
-const selectFruit = document.querySelector("#fruits");
-
-let test = "test";
-let test1 = "test1";
-let test2 = "test2";
-
-selectFruit.innerHTML = `<option value="test">${test}</option>`;
-selectFruit.innerHTML = `<option value="test">${test1}</option>`;
-selectFruit.innerHTML = `<option value="test">${test2}</option>`;
 
 
 getFruit();
+
+// console.log(ArrayFruit);
+
+// ArrayFruit.forEach(fruit => {
+//     console.log("test");
+//     // console.log(fruit.name);
+// })
