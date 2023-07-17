7d5c3447850f6418a7bbba0b6b0c2c0add50a485
Brian Bawden <brianbawden001@gmail.com>
2023-07-17 12:19:07 -0400

    heading grid alignment.

--- final/scripts/header.js
+++ final/scripts/header.js
@@ -1,24 +1,23 @@
-
 document.querySelector("header").innerHTML = `
 
-<div class="logo">
+<div id="logo">
   <picture
     ><img
-      class="icon"
+      id="icon"
       src="images/socialMedia/logo.png"
       alt="Carlsbad Fruit Stand logo"
     />
   </picture>
 </div>
-<div>
+<div id="socialMedia">
 <a href="https://www.facebook.com/"><img src="/images/socialMedia/facebook.png" alt="facebook logo"></a>
 <a href="https://www.instagram.com/"><img src="/images/socialMedia/instagram.png" alt="instagram logo"></a>
 <a href="https://twitter.com/"><img src="/images/socialMedia/twitter.png" alt="twitter logo"></a>
     
 </div>
-<div>
+<div id="pageLinks">
     <ul class="nav response">
-    <li><a href="#" onclick="toggleMenu()">&#9776;</a></li>
+    <!-- <li><a href="#" onclick="toggleMenu()">&#9776;</a></li> -->
     <li><a href="index.html">Home</a></li>
     <li><a href="fresh.html">Fresh</a></li>
     <li><a href="about.html">About Us</a></li>    
@@ -27,7 +26,7 @@ document.querySelector("header").innerHTML = `
 
   `;
 
-  document.querySelector("footer").innerHTML = `
+document.querySelector("footer").innerHTML = `
   <div id="footerAddress">
     <h2>Address</h2>
     <p><a href="https://www.google.com/maps/place/1921+Palomar+Oaks+Way,+Carlsbad,+CA+92008/@33.1211061,-117.2952398,17z/data=!3m1!4b1!4m15!1m8!3m7!1s0x80dc73453f3bee59:0xa4cb5592fcf65d2f!2sCarlsbad,+CA!3b1!8m2!3d33.1580933!4d-117.3505939!16zL20vMHI0d24!3m5!1s0x80dc73400cf16241:0x302b2613ec1f502f!8m2!3d33.1211062!4d-117.2906264!16s%2Fg%2F11c1xzbzml?entry=ttu">1921 Palomar Oaks Way</a><br>Carlsbad, CA 92008</p>
@@ -44,6 +43,7 @@ document.querySelector("header").innerHTML = `
     <p>info@carlsbadfruitstand.com</p>
   </div>
   <div class="footer-copyright">
+  <h2>Copyright</h2>
     <p id="copyright">
         &copy; <span id="copyYear"></span> .:|:.Brian Bawden.:|:. Canton, GA
     </p>
