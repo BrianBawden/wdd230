84263bbf1f136940c0f405d04b63d2a866bbecaa
Brian Bawden <brianbawden001@gmail.com>
2023-07-17 12:40:21 -0400

    add <hr> for clearification.

--- final/scripts/header.js
+++ final/scripts/header.js
@@ -27,6 +27,7 @@ document.querySelector("header").innerHTML = `
   `;
 
 document.querySelector("footer").innerHTML = `
+  <hr>
   <div id="footerAddress">
     <h2>Address</h2>
     <p><a href="https://www.google.com/maps/place/1921+Palomar+Oaks+Way,+Carlsbad,+CA+92008/@33.1211061,-117.2952398,17z/data=!3m1!4b1!4m15!1m8!3m7!1s0x80dc73453f3bee59:0xa4cb5592fcf65d2f!2sCarlsbad,+CA!3b1!8m2!3d33.1580933!4d-117.3505939!16zL20vMHI0d24!3m5!1s0x80dc73400cf16241:0x302b2613ec1f502f!8m2!3d33.1211062!4d-117.2906264!16s%2Fg%2F11c1xzbzml?entry=ttu">1921 Palomar Oaks Way</a><br>Carlsbad, CA 92008</p>
@@ -43,6 +44,7 @@ document.querySelector("footer").innerHTML = `
     <p>info@carlsbadfruitstand.com</p>
   </div>
   <div class="footer-copyright">
+  <hr>
   <h2>Copyright</h2>
     <p id="copyright">
         &copy; <span id="copyYear"></span> .:|:.Brian Bawden.:|:. Canton, GA
